d3fa845344553cf501fa039bbef5b74e78718401
tasya7777777 <tasya7777777@gmail.com>
2015-10-21 21:08:56 +0300

    comm

--- js.js
+++ js.js
@@ -2,20 +2,26 @@ function shoot(){
 	
 	document.getElementById("bullet").style.visibility = "visible";
 	
-
-	for(i = 0; i < 100; i++){
-		setTimeout(function(){ changeStyle(); }, 100);
-	}
 	
+	var elem1;
+	var style;
+	var pos = 0;
+	for (var i=0;i<=70;i++) {
+	   (function(ind) {
+		   setTimeout(function(){changeStyle();}, 50 + (30 * ind));
+	   })(i);
+	}
 	
 	
 	
 }
 function changeStyle(){
-	var elem1 = document.getElementById("bullet");
-	var style = window.getComputedStyle(elem1, null).getPropertyValue("left");
-	var pos = parseInt(style);
-	pos = pos + 10;
+	
+	elem1 = document.getElementById("bullet");
+	style = window.getComputedStyle(elem1, null).getPropertyValue("left");
+	pos = parseInt(style);
+	pos = pos + 20;
 	document.getElementById("bullet").style.left = pos.toString() + "px";
+	
 	
 }
